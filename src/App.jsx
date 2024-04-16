2f2a03bd3293eb78286cdd648cefa3e1dabe233c
Charles Knepshield <aknepshield@gmail.com>
2024-04-16 16:30:51 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Here we go!</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/services/gameService.jsx
@@ -0,0 +1,14 @@
+export const createGame = (gameObj) => {
+  return fetch(`http://localhost:8000/games`, {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${
+        JSON.parse(localStorage.getItem("rater_token")).token
+      }`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(gameObj),
+  });
+};
+
+// What token is this getting?
